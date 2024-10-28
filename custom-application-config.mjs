2088c07a00db873b196780404907c6535e032d8a
kavyakunder10 <148968829+kavyakunder10@users.noreply.github.com>
2024-10-28 15:12:17 +0530

    Add files via upload

---
+++ custom-application-config.mjs
@@ -0,0 +1,39 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptionsForCustomApplication}
+ */
+const config = {
+  name: 'Starter Fdf083',
+  entryPointUriPath,
+  cloudIdentifier: 'aws-eu',
+  env: {
+    development: {
+      initialProjectKey: 'sample-learning-app',
+    },
+    production: {
+      applicationId: 'cm2olklfi0001x71az26a8qd3',
+      url: 'https://your_app_hostname.com',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
