bd9dd3eba94ef50deb32e07a7273b7e87838a493
kavyakunder10 <kavya@compose.co.in>
2024-10-28 16:44:41 +0530

    new commit

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
-
+//this a comment
 // export const entryPointUriPath = 'starter-fdf083';
 
 export const entryPointUriPath =
