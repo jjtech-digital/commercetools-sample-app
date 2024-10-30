e66259b9127340920ce51ca1080554af2f05ec73
kavyakunder10 <kavya@compose.co.in>
2024-10-30 16:19:23 +0530

    update new changes

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,10 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
-//this is a comment
-// export const entryPointUriPath = 'starter-fdf083';
 
-export const entryPointUriPath =
-  typeof window === 'undefined'
-    ? process.env.ENTRY_POINT_URI_PATH
-    : window.app.entryPointUriPath;
+// export const entryPointUriPath =
+//   typeof window === 'undefined'
+//     ? process.env.ENTRY_POINT_URI_PATH
+// : window.app.entryPointUriPath;
+export const entryPointUriPath = 'starter-fdf083';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
